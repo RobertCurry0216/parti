cf30994804e2a1ab49971112c4636916f6d05461
robertcurry0216 <robertcurry0216@gmail.com>
2023-05-23 21:40:34 +1000

    more basic particles added

--- parti.lua
+++ parti.lua
@@ -34,6 +34,8 @@ function PartiSystem:update()
   local img = self._img
   img:clear(gfx.kColorClear)
   local offx, offy = gfx.getDrawOffset()
+  offx = offx - self.x
+  offy = offy - self.y
 
   gfx.pushContext(img)
     for i=l, 1, -1 do
@@ -58,6 +60,88 @@ function PartiSystem:spawnParticle(particle)
   self.count += 1
 end
 
+
 class("PartiParticle").extends()
 
 function PartiParticle:draw(age, offx, offy) end
+
+
+class("PartiImage").extends(PartiParticle)
+
+function PartiImage:init(x, y, image, options)
+  self.x = x
+  self.y = y
+  self.image = image
+  self.dx = options.dx or 0
+  self.dy = options.dy or 0
+  self.ax = options.ax or 0
+  self.ay = options.ay or 0
+  self.life = options.life or 300
+end
+
+function PartiImage:draw(age, offx, offy)
+  if age >= self.life then return true end
+
+  self.dx += self.ax
+  self.dy += self.ay
+
+  self.x += self.dx
+  self.y += self.dy
+
+  self.image:draw(self.x+offx, self.y+offy)
+end
+
+
+class("PartiImageTable").extends(PartiParticle)
+
+function PartiImageTable:init(x, y, table, options)
+  self.x = x
+  self.y = y
+  self.table = table
+  self.dx = options.dx or 0
+  self.dy = options.dy or 0
+  self.ax = options.ax or 0
+  self.ay = options.ay or 0
+  self.step = options.step or 1
+  self.frame_rate = options.frame_rate or 100
+  self.start = options.start or 1
+  self.final = options.final or #table+1
+end
+
+function PartiImageTable:draw(age, offx, offy)
+  local idx = self.start + self.step * (age // self.frame_rate)
+  if idx == self.final then return true end
+
+  self.dx += self.ax
+  self.dy += self.ay
+
+  self.x += self.dx
+  self.y += self.dy
+
+  self.table:getImage(idx):draw(self.x+offx, self.y+offy)
+end
+
+local circles <const> = gfx.imagetable.new(10)
+gfx.setColor(gfx.kColorWhite)
+for i=1, 10 do
+  local img = gfx.image.new(20, 20)
+  gfx.pushContext(img)
+    gfx.fillCircleAtPoint(10, 10, i)
+  gfx.popContext()
+  circles:setImage(i, img)
+end
+
+
+class("PartiFadingCircle").extends(PartiImageTable)
+
+function PartiFadingCircle:init(x, y, dx, dy, life)
+  PartiFadingCircle.super.init(self, x-10, y-10, circles, {
+    dx=dx,
+    dy=dy,
+    step=-1,
+    final=0,
+    start=life,
+  })
+end
+
+
